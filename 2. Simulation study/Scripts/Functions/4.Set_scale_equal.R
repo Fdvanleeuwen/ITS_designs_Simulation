f67c2f1f7a6d90480da070b24d059f0afd7a563c
Fdvanleeuwen <94541170+Fdvanleeuwen@users.noreply.github.com>
2023-05-15 17:55:26 +0200

    Add comments

--- 2. Simulation study/Scripts/Functions/4.Set_scale_equal.R
+++ 2. Simulation study/Scripts/Functions/4.Set_scale_equal.R
@@ -1,10 +1,15 @@
+# Check https://www.zachburchill.ml/constant_scales/ for a walkthrough. 
 
+# Given a plot and a scale, get the range of the values used in the scale for that plot
+#   Not really intended for users
 simple_range_extracter <- function(p, scale) {
   d <- ggplot_build(p)
   d$plot$scales$get_scales(scale$aesthetics)$range$range
 }
 
-
+# Given a bunch of plots and a scale you want to apply to them, this returns that scale,
+#   but with limits that encompass the union of all the ranges of values in those plots
+# Meant to be for users
 get_shared_scale <- function(..., scale) {
   plots <- list(...)
   ranges <- purrr::map(plots, ~simple_range_extracter(., scale))
@@ -13,7 +18,11 @@ get_shared_scale <- function(..., scale) {
   scale
 }
 
-# Main function
+# Given the unquoted variable names of a bunch of plots that have been 
+#   saved to the current environment, and a scale that you want to apply
+#   to them, this will call `get_shared_scale()` using those plots and scale
+#   add that scale to the plots, and then assign these new plots to those 
+#   variable names, essentially editing the plots 'in place'
 set_scale_union <- function(..., scale) {
   exprs <- rlang::enexprs(...)
   scale <- get_shared_scale(..., scale = scale)
